75591cbba712a6cef467da36eaed17795ea85893
annette.findley <annette.varndell@wagglebee.net>
2025-07-25 14:36:38 -0400

    test

--- src/AspireMe.AppHost/AppHost.cs
+++ src/AspireMe.AppHost/AppHost.cs
@@ -13,7 +13,6 @@ var dbServer = builder.AddPostgres( "postgres", userName: dbUser, password: dbPa
     .WithPgAdmin( x => x.WithImageTag( "9.5" ) );
 
 
-
 var projectdb = dbServer.AddDatabase( "medical" );
 
 var apiService = builder.AddProject<Projects.AspireMe_Api>( "aspireme-api" )
